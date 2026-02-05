8ef6d932f7c060160d811f2d050f673cf36c6cf2
Abhishek <abhisheksai1122@gmail.com>
2026-02-06 01:44:47 +0530

    Refactored the code

--- src/main/java/com/abhi/orders/exceptions/ResourceValidationException.java
+++ src/main/java/com/abhi/orders/exceptions/ResourceValidationException.java
@@ -2,13 +2,12 @@ package com.abhi.orders.exceptions;
 
 import lombok.Getter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class ResourceValidationException extends RuntimeException {
 
-    List<String> errorList = new ArrayList<>();
+    List<String> errorList;
 
     public ResourceValidationException(String message, List<String> errorList) {
         super(message);
